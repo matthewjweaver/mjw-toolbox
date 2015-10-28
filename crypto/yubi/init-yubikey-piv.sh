aa93521d3920d83a1601262a890c39193df46359
matthew j weaver <matt@ice-nine.org>
2015-10-28 07:25:39 -0400

    urandom 4 lyfe (h/t to @alex)

--- crypto/yubi/init-yubikey-piv.sh
+++ crypto/yubi/init-yubikey-piv.sh
@@ -1,8 +1,8 @@
-key=`dd if=/dev/random bs=1 count=24 2>/dev/null | hexdump -v -e '/1 "%02X"'`
+key=`dd if=/dev/urandom bs=1 count=24 2>/dev/null | hexdump -v -e '/1 "%02X"'`
 echo $key > yubico-internal-https-$USER-key.txt
-pin=`dd if=/dev/random bs=1 count=6 2>/dev/null | hexdump -v -e '/1 "%u"'|cut -c1-6`
+pin=`dd if=/dev/urandom bs=1 count=6 2>/dev/null | hexdump -v -e '/1 "%u"'|cut -c1-6`
 echo $pin > yubico-internal-https-$USER-pin.txt
-puk=`dd if=/dev/random bs=1 count=6 2>/dev/null | hexdump -v -e '/1 "%u"'|cut -c1-8`
+puk=`dd if=/dev/urandom bs=1 count=6 2>/dev/null | hexdump -v -e '/1 "%u"'|cut -c1-8`
 echo $puk > yubico-internal-https-$USER-puk.txt
 yubico-piv-tool -a set-mgm-key -n $key
 yubico-piv-tool -k $key -a change-pin -P 123456 -N $pin
