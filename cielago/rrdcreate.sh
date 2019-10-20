e44c111fb6024146608745fb9adc2c721fd39790
matthewjweaver <matt@ice-nine.org>
2019-10-20 12:59:16 -0400

    add update script, modify rrdcreate to auto-select labels

--- cielago/rrdcreate.sh
+++ cielago/rrdcreate.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
-# Picked a moment in time that all data should date from.
+# Picked a moment in time that all data must be after.
 # Sun Oct 20 11:10:38 EDT 2019
 RRD_EPOCH="1571584238"
 
-# 
-
-# There is a pair of labels on all traffic at the edge of the network,
-# named for the host and direction the traffic is travelling.
-labels_in="96.73.134.169-in 96.73.134.170-in 96.73.134.171-in 96.73.134.172-in 96.73.134.173-in"
-labels_out="96.73.134.169-out 96.73.134.170-out 96.73.134.171-out 96.73.134.172-out 96.73.134.173-out"
+# This script has two assumptions about pf rules:
+#   1. There are a pair of labels on all traffic at the edge of the
+#      network, named for the host and direction the traffic is
+#      travelling. For example: 192.168.1.1-in and 192.168.1.1-out
+#   2. The pf rules with these labels are set "no state"
 
 # The labels each get eight per-label statistics provided via pfctl(8):
 #   evaluations,
@@ -20,13 +19,14 @@ labels_out="96.73.134.169-out 96.73.134.170-out 96.73.134.171-out 96.73.134.172-
 #   packets out,
 #   bytes out,
 #   state creations
-# The rules do not create states as of $RRD_EPOCH, so we expect the "in"
-# labels to only show packets/bytes in, and so on with the "out" rules.
-# State creations should always be 0.
+
+# The rules do not create states, so we expect the "in" labels to only
+# show packets/bytes in, and so on with the "out" rules.
+# Also, State creations should always be 0.
 # We create DS and RRAs for them anyway, because it's cheap and who
 # knows what the future holds.
 
-for label in $labels_in $labels_out; do
+for label in $(pfctl -s label|cut -d ' ' -f 1); do
   # We create one RRD per label.
   RRD_CMD="rrdtool create ${label}.rrd"
 
@@ -35,23 +35,26 @@ for label in $labels_in $labels_out; do
   RRD_CMD="${RRD_CMD} -d unix:/var/run/rrd/rrdcached.sock"
 
   # Assume we'll collect stats at 5Hz, and never miss more than 5
-  # seconds of measurement (heartbeat 5s, below).
+  # seconds of measurement (heartbeat 5s on DS lines, below).
   RRD_CMD="${RRD_CMD} -b ${RRD_EPOCH} -s 200ms"
 
-  # assume we will never see more than 2m pps, and never measure less
+  # Assume we will never see more than 2m pps, and never measure less
   # than 5 seconds apart. so, 10 million packets each way or 20 million
   # states (and evalutions).
+
+  # Assume we will never be > 1Gbps, never measure less than 5 seconds
+  # apart: 5 gigabits = 6.25e+08 bytes
+
+  # DS are created in the order pfctl outputs them, for ease of
+  # updating.
   RRD_CMD="${RRD_CMD} DS:evaluations:ABSOLUTE:5s:0:20000000"
   RRD_CMD="${RRD_CMD} DS:packets_total:ABSOLUTE:5s:0:20000000"
-  RRD_CMD="${RRD_CMD} DS:packets_in:ABSOLUTE:5s:0:10000000"
-  RRD_CMD="${RRD_CMD} DS:packets_out:ABSOLUTE:5s:0:10000000"
-  RRD_CMD="${RRD_CMD} DS:state_creations:ABSOLUTE:5s:0:20000000"
-
-  # assume we will never be > 1Gbps, never measure less than 5 seconds
-  # apart: 5 gigabits = 6.25e+08 bytes
   RRD_CMD="${RRD_CMD} DS:bytes_total:ABSOLUTE:5s:0:625000000"
+  RRD_CMD="${RRD_CMD} DS:packets_in:ABSOLUTE:5s:0:10000000"
   RRD_CMD="${RRD_CMD} DS:bytes_in:ABSOLUTE:5s:0:625000000"
+  RRD_CMD="${RRD_CMD} DS:packets_out:ABSOLUTE:5s:0:10000000"
   RRD_CMD="${RRD_CMD} DS:bytes_out:ABSOLUTE:5s:0:625000000"
+  RRD_CMD="${RRD_CMD} DS:state_creations:ABSOLUTE:5s:0:20000000"
 
   # keep full resolution for 1 week
   # keep secondly resolution for 28 days
@@ -64,5 +67,5 @@ for label in $labels_in $labels_out; do
   RRD_CMD="${RRD_CMD} RRA:AVERAGE:0.5:3600:8760"
   RRD_CMD="${RRD_CMD} RRA:AVERAGE:0.5:43200:23360"
 
-  time ${RRD_CMD}
+  echo time ${RRD_CMD}
 done
---
+++ cielago/rrdupdate.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# This script assumes a few things:
+#   0. pf labels end with "in" or "out"
+#   1. There is an rrd file for each pf label
+#   2. The files are named "<label name>.rrd"
+#   3. The rrd files were created with the DS sources in the same order
+#      as the pfctl -s label output.
+#   4. rrdcached is running with the OpenBSD default socket location
+#
+# 1-3 are easily satisfied by using the rrdcreate.sh file in this
+# repository.
+
+NOW=$(date +%s)
+
+# This resets the counters after collecting the stats.
+/sbin/pfctl -zs label |
+  sed \
+    -e's/ /:/g' \
+    -e's/^/rrdtool update /g' \
+    -e"s%in:%in.rrd -d unix:/var/run/rrd/rrdcached.sock ${NOW}:%g" \
+    -e"s%out:%out.rrd -d unix:/var/run/rrd/rrdcached.sock ${NOW}:%g"
