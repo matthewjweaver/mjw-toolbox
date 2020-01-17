8155bd423ad2d3cb03eaff765bd506db5fea9bf9
matthewjweaver <matt@ice-nine.org>
2020-01-17 22:29:42 +0000

    tighten rrdstats loop

--- cielago/rrdstatsloop.sh
+++ cielago/rrdstatsloop.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-while sleep 4; do
-  doas /home/sysop/bin/rrdupdate.sh | \
-    /usr/bin/logger -cf - -t rrdstats
+while sleep 2; do
+  doas /home/sysop/bin/rrdupdate.sh | 2>&1 \
+    /usr/bin/logger -ct rrdstats
 done
