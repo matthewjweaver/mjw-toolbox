04ed34dfc12a2b5daa294700330afa86a8c3b27b
matthewjweaver <matt@ice-nine.org>
2020-04-13 17:10:26 +0000

    switch to collecting stats on various traffic types

--- cielago/rrdgraph.sh
+++ cielago/rrdgraph.sh
@@ -32,22 +32,46 @@ bps_graph() {
   RRD_CMD="${RRD_CMD} --lower-limit -${MAX_BW_DL}"
 
   # bytes, in
-  RRD_CMD="${RRD_CMD} DEF:tabr-B-in=/var/db/rrd/em0-in.rrd:bytes_in:AVERAGE"
+  RRD_CMD="${RRD_CMD} DEF:ssh-B-in=/var/db/rrd/em0-22-in.rrd:bytes_in:AVERAGE"
+  RRD_CMD="${RRD_CMD} DEF:dns-B-in=/var/db/rrd/em0-53-in.rrd:bytes_in:AVERAGE"
+  RRD_CMD="${RRD_CMD} DEF:http-B-in=/var/db/rrd/em0-80-in.rrd:bytes_in:AVERAGE"
+  RRD_CMD="${RRD_CMD} DEF:https-B-in=/var/db/rrd/em0-443-in.rrd:bytes_in:AVERAGE"
+  RRD_CMD="${RRD_CMD} DEF:other-B-in=/var/db/rrd/em0-other-in.rrd:bytes_in:AVERAGE"
 
   # bytes, out
-  RRD_CMD="${RRD_CMD} DEF:tabr-B-out=/var/db/rrd/em0-out.rrd:bytes_out:AVERAGE"
+  RRD_CMD="${RRD_CMD} DEF:ssh-B-out=/var/db/rrd/em0-22-out.rrd:bytes_out:AVERAGE"
+  RRD_CMD="${RRD_CMD} DEF:dns-B-out=/var/db/rrd/em0-53-out.rrd:bytes_out:AVERAGE"
+  RRD_CMD="${RRD_CMD} DEF:http-B-out=/var/db/rrd/em0-80-out.rrd:bytes_out:AVERAGE"
+  RRD_CMD="${RRD_CMD} DEF:https-B-out=/var/db/rrd/em0-443-out.rrd:bytes_out:AVERAGE"
+  RRD_CMD="${RRD_CMD} DEF:other-B-out=/var/db/rrd/em0-other-out.rrd:bytes_out:AVERAGE"
 
   # bps in, made negative so it will graph "down"
-  RRD_CMD="${RRD_CMD} CDEF:tabr-b-in=0,tabr-B-in,-,8,*"
+  RRD_CMD="${RRD_CMD} CDEF:ssh-b-in=0,ssh-B-in,-,8,*"
+  RRD_CMD="${RRD_CMD} CDEF:dns-b-in=0,dns-B-in,-,8,*"
+  RRD_CMD="${RRD_CMD} CDEF:http-b-in=0,http-B-in,-,8,*"
+  RRD_CMD="${RRD_CMD} CDEF:https-b-in=0,https-B-in,-,8,*"
+  RRD_CMD="${RRD_CMD} CDEF:other-b-in=0,other-B-in,-,8,*"
   
   # bps out
-  RRD_CMD="${RRD_CMD} CDEF:tabr-b-out=tabr-B-out,8,*"
+  RRD_CMD="${RRD_CMD} CDEF:ssh-b-out=ssh-B-out,8,*"
+  RRD_CMD="${RRD_CMD} CDEF:dns-b-out=dns-B-out,8,*"
+  RRD_CMD="${RRD_CMD} CDEF:http-b-out=http-B-out,8,*"
+  RRD_CMD="${RRD_CMD} CDEF:https-b-out=https-B-out,8,*"
+  RRD_CMD="${RRD_CMD} CDEF:other-b-out=other-B-out,8,*"
   
   # graph bandwidth in
-  RRD_CMD="${RRD_CMD} AREA:tabr-b-in${cyan}:sietchtabr_bps:STACK"
+  RRD_CMD="${RRD_CMD} AREA:ssh-b-in${orange}:ssh_bps:STACK"
+  RRD_CMD="${RRD_CMD} AREA:dns-b-in${blue}:dns_bps:STACK"
+  RRD_CMD="${RRD_CMD} AREA:http-b-in${red}:http_bps:STACK"
+  RRD_CMD="${RRD_CMD} AREA:https-b-in${magenta}:https_bps:STACK"
+  RRD_CMD="${RRD_CMD} AREA:other-b-in${cyan}:other_bps:STACK"
   
   # graph bandwidth out (no legend for these, same colors as above)
-  RRD_CMD="${RRD_CMD} AREA:tabr-b-out${cyan}::STACK"
+  RRD_CMD="${RRD_CMD} AREA:ssh-b-out${orange}::STACK"
+  RRD_CMD="${RRD_CMD} AREA:dns-b-out${blue}::STACK"
+  RRD_CMD="${RRD_CMD} AREA:http-b-out${red}::STACK"
+  RRD_CMD="${RRD_CMD} AREA:https-b-out${magenta}::STACK"
+  RRD_CMD="${RRD_CMD} AREA:other-b-out${cyan}::STACK"
 
   doas nice $RRD_CMD > /dev/null
 }
@@ -56,19 +80,39 @@ pps_graph() {
   RRD_CMD="${1}"
 
   # packets, in
-  RRD_CMD="${RRD_CMD} DEF:tabr-p-in=/var/db/rrd/em0-in.rrd:packets_in:MAX"
+  RRD_CMD="${RRD_CMD} DEF:ssh-p-in=/var/db/rrd/em0-22-in.rrd:packets_in:AVERAGE"
+  RRD_CMD="${RRD_CMD} DEF:dns-p-in=/var/db/rrd/em0-53-in.rrd:packets_in:AVERAGE"
+  RRD_CMD="${RRD_CMD} DEF:http-p-in=/var/db/rrd/em0-80-in.rrd:packets_in:AVERAGE"
+  RRD_CMD="${RRD_CMD} DEF:https-p-in=/var/db/rrd/em0-443-in.rrd:packets_in:AVERAGE"
+  RRD_CMD="${RRD_CMD} DEF:other-p-in=/var/db/rrd/em0-other-in.rrd:packets_in:AVERAGE"
 
   # packets, out
-  RRD_CMD="${RRD_CMD} DEF:tabr-pps-out=/var/db/rrd/em0-out.rrd:packets_out:MAX"
+  RRD_CMD="${RRD_CMD} DEF:ssh-pps-out=/var/db/rrd/em0-22-out.rrd:packets_out:AVERAGE"
+  RRD_CMD="${RRD_CMD} DEF:dns-pps-out=/var/db/rrd/em0-53-out.rrd:packets_out:AVERAGE"
+  RRD_CMD="${RRD_CMD} DEF:http-pps-out=/var/db/rrd/em0-80-out.rrd:packets_out:AVERAGE"
+  RRD_CMD="${RRD_CMD} DEF:https-pps-out=/var/db/rrd/em0-443-out.rrd:packets_out:AVERAGE"
+  RRD_CMD="${RRD_CMD} DEF:other-pps-out=/var/db/rrd/em0-other-out.rrd:packets_out:AVERAGE"
 
   # packets in, made negative so it will graph "down"
-  RRD_CMD="${RRD_CMD} CDEF:tabr-pps-in=0,tabr-p-in,-"
+  RRD_CMD="${RRD_CMD} CDEF:ssh-pps-in=0,ssh-p-in,-"
+  RRD_CMD="${RRD_CMD} CDEF:dns-pps-in=0,dns-p-in,-"
+  RRD_CMD="${RRD_CMD} CDEF:http-pps-in=0,http-p-in,-"
+  RRD_CMD="${RRD_CMD} CDEF:https-pps-in=0,https-p-in,-"
+  RRD_CMD="${RRD_CMD} CDEF:other-pps-in=0,other-p-in,-"
 
   # graph packets in
-  RRD_CMD="${RRD_CMD} LINE:tabr-pps-in${cyan}:sietchtabr_pps"
+  RRD_CMD="${RRD_CMD} LINE:ssh-pps-in${orange}:ssh_pps"
+  RRD_CMD="${RRD_CMD} LINE:dns-pps-in${blue}:dns_pps"
+  RRD_CMD="${RRD_CMD} LINE:http-pps-in${red}:http_pps"
+  RRD_CMD="${RRD_CMD} LINE:https-pps-in${magenta}:https_pps"
+  RRD_CMD="${RRD_CMD} LINE:other-pps-in${cyan}:other_pps"
 
   # graph packets out
-  RRD_CMD="${RRD_CMD} LINE:tabr-pps-out${cyan}"
+  RRD_CMD="${RRD_CMD} LINE:ssh-pps-out${orange}"
+  RRD_CMD="${RRD_CMD} LINE:dns-pps-out${blue}"
+  RRD_CMD="${RRD_CMD} LINE:http-pps-out${red}"
+  RRD_CMD="${RRD_CMD} LINE:https-pps-out${magenta}"
+  RRD_CMD="${RRD_CMD} LINE:other-pps-out${cyan}"
 
   doas nice $RRD_CMD > /dev/null
 }
