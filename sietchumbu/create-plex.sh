475ea6e1225d849905f0033bfa0c6335bf282823
matthewjweaver <matt@ice-nine.org>
2020-06-20 14:57:47 +0000

    plex docker creation script

---
+++ sietchumbu/create-plex.sh
@@ -0,0 +1,14 @@
+docker run \
+  -d \
+  --name plex \
+  --network=host \
+  -e TZ="UTC" \
+  -e PLEX_UID="501" \
+  -e PLEX_GID="100" \
+  -e PLEX_CLAIM="XXXLOLXXX" \
+  -v /home/mistakenot/plex-meta/config:/config \
+  -v /home/mistakenot/plex-meta/transcode:/transcode \
+  -v /home/mistakenot:/mistakenot \
+  -v /home/multi:/multi \
+  --device=/dev/dri:/dev/dri \
+  plexinc/pms-docker
