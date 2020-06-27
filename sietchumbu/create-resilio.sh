3ac9096e6c33c17755f27e27580ca4ca94bf62f5
matthewjweaver <matt@ice-nine.org>
2020-06-27 10:29:22 +0000

    resilio container draft

---
+++ sietchumbu/create-resilio.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+CONTAINER=resilio/sync
+docker stop resilio
+docker rm resilio
+docker pull ${CONTAINER}
+docker run \
+  -d \
+  --name resilio \
+  --restart always \
+  -e PUID="501" \
+  -e PGID="100" \
+  -p 8888 \
+  -p 55555 \
+  -v /home/mistakenot/resilio-meta:/mnt/sync \
+  -v /home/mistakenot:/mnt/mounted_folders/mistakenot \
+  -v /home/multi:/mnt/mounted_folders/multi \
+  ${CONTAINER}
+docker prune
