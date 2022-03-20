e85cac175d3f52e49986a28b855e481e517bd83f
matthew j weaver <matt@ice-nine.org>
2022-03-20 15:12:35 -0500

    Support for retrieving secrets from 1Password (#213)
    * sync iked.conf
    
    * Draft of prompting for secrets on install of files
    
    * Updated draft which uses the 1password CLI.
    
    * trim cruft from install-secretfied
    
    * cleanup rest of secrets

---
+++ install-secretfied.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# Usage:
+#  install-secretfied.sh <input> <install(1) flags> <target>
+#
+# This script will create a tmp file as its output, and use install(1)
+# to copy that file to the specified target.
+#
+# This script is expected to be run via the 1Password CLI's "run"
+# command, ie 'op run -- sh install-secretfied.sh'.
+#
+# For each string in the form XXX_SOMETHING_XXX in the input file, the
+# script assumes the presence of a matching env variable containing a
+# secret fetched by op. See .env files near the Makefiles for a mapping
+# of variable names to vault locations.
+#
+# Finally, the script removes the temporary file.
+#
+# This script will break badly if a secret contains an '@', since that
+# is what the script uses as a field delimiter for the sed command.
+
+#set -x
+set -e
+
+if ! [[ -f "$1" ]]; then
+  echo "Usage: "
+  echo '  install-secretfied.sh <input> <install(1) flags> <target>'
+  exit 1
+fi
+
+TMPFILE=`mktemp` || exit 1
+cp $1 $TMPFILE
+
+for TOKEN in $(grep XXX $TMPFILE |sed -e's/^[^X]*XXX_/XXX_/g' -e's/_XXX[^_]*$/_XXX/g'); do
+  eval "SECRET=\$$TOKEN"
+  sed -i -e "s@$TOKEN@$SECRET@" $TMPFILE
+done
+/usr/bin/doas /usr/bin/install $2 $TMPFILE $3
+/bin/rm $TMPFILE
