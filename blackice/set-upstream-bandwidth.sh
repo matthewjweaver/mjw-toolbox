f6a0bee3eb8cfe4a20b30ac9e4bb07e11351f8b4
matthewjweaver <matt@ice-nine.org>
2022-08-02 16:03:39 -0400

    Fix bug in logic, previously never worked after 2300h until probably the 0100h cron run.

--- blackice/set-upstream-bandwidth.sh
+++ blackice/set-upstream-bandwidth.sh
@@ -5,6 +5,9 @@ set -e
 # /etc/pf.conf.
 
 # These hours are firewall's local timezone
+# Beginning at PEAK_START_HOUR, and ending at PEAK_END_HOUR, the
+# firewall's uplink interface will be set to $upstream_peak bandwidth.
+# Otherwise, it will be set to $upstream_offpeak bandwidth.
 PEAK_START_HOUR="16"
 PEAK_END_HOUR="23"
 
@@ -25,15 +28,13 @@ fi
 #   "$upstream_"
 # with either $upstream_peak or $upstream_offpeak, depending on the hour
 # of the day.
-if [ $HOUR_NOW -ge $PEAK_START_HOUR ]; then
-  if [ $HOUR_NOW -le $PEAK_END_HOUR ]; then
-    # The current hour is inside peak times
-    ed -s /etc/pf.conf << EOF
+if [ $HOUR_NOW -ge $PEAK_START_HOUR ] && [ $HOUR_NOW -lt $PEAK_END_HOUR ]; then
+  # The current hour is inside peak times
+  ed -s /etc/pf.conf << EOF
 /^queue fq on [$]uplink/s/[$]upstream_[^ ]*/\$upstream_peak/g
 w $OUTPUT
 q
 EOF
-  fi
 else
   # the current hour is outside peak times
   ed -s /etc/pf.conf << EOF
