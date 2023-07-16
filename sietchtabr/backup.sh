b691ccfa9970be30158238756e7069c366882922
matthewjweaver <matt@ice-nine.org>
2023-07-16 14:17:15 +0000

    tabr: backup.sh usability and error handling improvements

--- sietchtabr/backup.sh
+++ sietchtabr/backup.sh
@@ -14,24 +14,41 @@ usage() {
   echo
   echo "Example:"
   echo "  backup.sh korba.nodeless.net"
+  echo
+  echo "Requires authenticated 1password."
+  echo "For instance, eval $(op signin) before executing."
 }
 
 if ! op whoami 2>&1 > /dev/null; then
-  echo "Requires authenticated 1password."
-  echo "Perhaps, eval $(op signin) first."
+  echo "Unable to confirm that op (1password) is available."
+  echo
+  usage
   exit
 fi
 
 TARGET_HOST=${1}
+if [ -z ${TARGET_HOST} ]; then
+  usage
+  exit
+fi
+
 TARGET_USER=$(op read op://Personal/${TARGET_HOST}/admin)
-if ! [ -z ${TARGET_HOST} ] && ! [ -z ${TARGET_USER} ]; then
+if ! [ -z ${TARGET_USER} ]; then
   echo "Will login as ${TARGET_USER} to ${TARGET_HOST}."
 else
+  echo "Unable to find target user on host using op."
+  echo
   usage
   exit
 fi
 
 PASSWORD=$(op read op://Personal/${TARGET_HOST}/dump-chacha-key)
+if [ -z ${PASSWORD} ]; then
+  echo "Cannot find encryption password for target host using op."
+  echo
+  usage
+  exit
+fi
 
 BACKUPS_ROOT=/arc/obsd-bak/dumps/${TARGET_HOST}
 if ! [ -d ${BACKUPS_ROOT} ]; then
