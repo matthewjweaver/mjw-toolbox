b98949d08e855b980be7761ec01766ef79edc681
matthewjweaver <matt@ice-nine.org>
2023-07-19 20:34:04 +0000

    tabr: restore-backup.sh draft

--- sietchtabr/restore-backup.sh
+++ sietchtabr/restore-backup.sh
@@ -3,23 +3,34 @@ set -e
 umask 027
 
 # TODO(mjw)
-#   1. make work for a single host & most recent dump
+#   1. make work via an openbsd ramdisk kernel
 
 # This entire thing depends on the 1password CLI, op.
 
+# Since the OpenBSD ramdisk doesn't have either ssh or libressl on it, I
+# couldn't figure out a praxis for restoring using only the ramdisk.
+# Therefore, this script has a few prerequisites:
+#   1. The target system disk exists as /dev/sd1
+#   2. The target system disk has sufficient size to support the backed
+#   up disklabel.
+#
+# In general, the assumption is that this script is run on a host like
+# axlotl.tank.int.nodeless.net, with the target disk as sd1.
+
+
 usage() {
   echo "Usage:"
-  echo "  backup.sh <hostname>"
+  echo "  backup.sh <hostname> <backup number> <disk UUID>"
   echo
   echo "Example:"
-  echo "  backup.sh korba.nodeless.net"
+  echo "  backup.sh korba.nodeless.net c99a329534106e59 num-1"
   echo
   echo "Requires authenticated 1password."
-  echo "For instance, eval $(op signin) before executing."
+  echo 'For instance, eval $(op signin) before executing.'
 }
 
 if ! op whoami 2>&1 > /dev/null; then
-  echo "Unable to confirm that op (1password) is available."
+  echo "ERROR: Unable to confirm that op (1password) is available."
   usage
   exit
 fi
@@ -30,19 +41,13 @@ if [ -z ${TARGET_HOST} ]; then
   exit
 fi
 
-TARGET_USER=$(op read op://Personal/${TARGET_HOST}/admin)
-if ! [ -z ${TARGET_USER} ]; then
-  echo "Will login as ${TARGET_USER} to ${TARGET_HOST}."
-else
-  echo "Unable to find target user on host using op."
-  echo
-  usage
-  exit
-fi
+# We assume the machine is a more or less new install, with only a root
+# user.
+TARGET_USER=root
 
 PASSWORD=$(op read op://Personal/${TARGET_HOST}/dump-chacha-key)
 if [ -z ${PASSWORD} ]; then
-  echo "Cannot find encryption password for target host using op."
+  echo "ERROR: Cannot find encryption password for target host using op."
   echo
   usage
   exit
@@ -50,11 +55,29 @@ fi
 
 BACKUPS_ROOT=/arc/obsd-bak/dumps/${TARGET_HOST}
 if ! [ -d ${BACKUPS_ROOT} ]; then
-  echo "${BACKUPS_ROOT} doesn't exist, exiting."
+  echo "ERROR: ${BACKUPS_ROOT} doesn't exist, exiting."
+  echo
+  usage()
   exit
 fi
 
-# probably:
-# disklabel the drive
-# for each dump, restore whole thing to partition
-# maybe: in parallel?
+BACKUP_DIR=${BACKUPS_ROOT}/lvl-0-${2}
+if ! [ -d ${BACKUP_DIR} ]; then
+  echo "ERROR: ${BACKUP_DIR} doesn't exist, exiting."
+  echo "available backups:"
+  ls ${BACKUPS_ROOT}
+  echo
+  usage()
+  exit
+fi
+
+for DUMP in $(ls ${BACKUP_DIR}/*chacha); do
+  PARTITION=$(basename ${DUMP} .gz.chacha)
+  TARGET_DIR=$(< ${BACKUP_DIR}/fstab awk "/${PARTITION}/ {print "'$2}')
+  # We ssh with StrictHostKeyChecking=no because we haven't restored the
+  # ssh host keys from backup yet.
+  #< $DUMP /usr/bin/openssl enc -d -chacha -iter 1000000 -k "${PASSWORD}" |\
+  #gunzip - |\
+  #ssh -o StrictHostKeyChecking=no root@${TARGET_HOST} "set -e; cd ${TARGET_DIR}; restore ryf -" 
+done
+
