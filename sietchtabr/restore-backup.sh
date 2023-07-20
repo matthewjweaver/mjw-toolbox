4cda89813af550069031bf15e3b9cee49894a00f
matthew j weaver <matt@ice-nine.org>
2023-07-19 20:06:47 -0500

    Restore tools, and a VM for executing backup restorations. (#261)
    * tabr: backup.sh usability and error handling improvements
    
    * tabr: garbage draft outline of restore-backup.sh
    
    * tabr: add bridge on LAN
    
    * tabr: welcome axlotl-tank
    
    * tabr: missing vm.conf makefile dep
    
    * tabr: restore-backup.sh draft
    
    * tabr, axlotl-tank: restore-backup.sh seems to work.

---
+++ sietchtabr/restore-backup.sh
@@ -0,0 +1,117 @@
+#!/bin/sh
+set -e
+umask 027
+
+# TODO(mjw)
+#   1. make work via an openbsd ramdisk kernel
+#   1. maybe make this not such a giant footgun?
+
+# This entire thing depends on the 1password CLI, op.
+
+# Since the OpenBSD ramdisk doesn't have either ssh or libressl on it, I
+# couldn't figure out a praxis for restoring using only the ramdisk.
+# Therefore, this script has a few prerequisites:
+#   1. The target system disk exists as /dev/sd1
+#   2. The target system disk has sufficient size to support the backed
+#   up disklabel.
+#
+# In general, the assumption is that this script is run on a host like
+# axlotl.tank.int.nodeless.net, with the target disk as sd1.
+
+
+usage() {
+  echo "Usage:"
+  echo "  backup.sh <hostname> <backup number> <target disk>"
+  echo
+  echo "Example:"
+  echo "  backup.sh korba.nodeless.net num-1 sd1"
+  echo
+  echo "Requires authenticated 1password."
+  echo 'For instance, eval $(op signin) before executing.'
+}
+
+if ! op whoami 2>&1 > /dev/null; then
+  echo "ERROR: Unable to confirm that op (1password) is available."
+  usage
+  exit 1
+fi
+
+TARGET_HOST=${1}
+if [ -z ${TARGET_HOST} ]; then
+  usage
+  exit 1
+fi
+
+# We assume the machine is a more or less new install, with only a root
+# user.
+TARGET_USER=root
+
+PASSWORD=$(op read op://Personal/${TARGET_HOST}/dump-chacha-key)
+if [ -z ${PASSWORD} ]; then
+  echo "ERROR: Cannot find encryption password for target host using op."
+  echo
+  usage
+  exit 1
+fi
+
+BACKUPS_ROOT=/arc/obsd-bak/dumps/${TARGET_HOST}
+if ! [ -d ${BACKUPS_ROOT} ]; then
+  echo "ERROR: ${BACKUPS_ROOT} doesn't exist, exiting."
+  echo
+  usage()
+  exit 1
+fi
+
+BACKUP_DIR=${BACKUPS_ROOT}/lvl-0-${2}
+if ! [ -d ${BACKUP_DIR} ]; then
+  echo "ERROR: ${BACKUP_DIR} doesn't exist, exiting."
+  echo "available backups:"
+  ls ${BACKUPS_ROOT}
+  echo
+  usage()
+  exit 1
+fi
+
+DISKLABEL=${BACKUP_DIR}/disklabel-*
+if ! [ -f ${DISKLABEL} ]; then
+  echo "ERROR: ${DISKLABEL} doesn't exist, exiting."
+  echo
+  usage()
+  exit 1
+fi
+
+TARGET_DISK=${3}
+if ! [ -z $(doas /sbin/mount|grep ${TARGET_DISK}) ]; then
+  echo "ERROR: It appears ${TARGET_DISK} is mounted."
+  echo
+  usage()
+  exit 1
+fi
+
+if ! [ -z $(doas /sbin/mount|grep /mnt) ]; then
+  echo "ERROR: It appears /mnt is mounted."
+  echo
+  usage()
+  exit 1
+fi
+
+# OK, from here out we are shooting without looking further.
+
+doas /sbin/disklabel -R ${TARGET_DISK} ${DISKLABEL}
+
+# A grand assumption here is that / is partition a every time, and
+# therefore comes first!
+for DUMP in $(ls ${BACKUP_DIR}/*chacha); do
+  PARTITION=$(basename ${DUMP} .gz.chacha)
+  TARGET_DIR=$(< ${BACKUP_DIR}/fstab awk "/${PARTITION}/ {print "'$2}')
+  # Strip the disk uuid from the partition
+  PARTITION=$(echo ${PARTITION}|sed -e's/[^.]*[.]//g')
+  echo "Restoring ${DUMP} to ${TARGET_DIR}."
+  doas /sbin/newfs /dev/r${TARGET_DISK}${PARTITION}
+  doas /sbin/mount /dev/${TARGET_DISK}${PARTITION} /mnt${TARGET_DIR}
+  cd /mnt${TARGET_DIR}
+  < $DUMP /usr/bin/openssl enc -d -chacha -iter 1000000 -k "${PASSWORD}" |\
+    /usr/bin/gunzip - |\
+    doas /sbin/restore ryf -
+done
+
