de6d4e93b942f33a53f0ce15a5b51cd73c47b818
matthewjweaver <matt@ice-nine.org>
2023-12-11 12:48:38 +0000

    umbu: jellyfin

--- sietchumbu/upgrade-all.sh
+++ sietchumbu/upgrade-all.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+./create-jellyfin.sh
+./create-homeassistant.sh
 ./create-plex.sh
 ./create-radarr.sh
 ./create-resilio.sh
 ./create-sabnzbd.sh
 ./create-sonarr.sh
-./create-homeassistant.sh
 docker system prune
