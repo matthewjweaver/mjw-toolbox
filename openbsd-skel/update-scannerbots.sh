73be9d27c14bd5cbaa2e5baa8253d7cc1e7e70f0
matthewjweaver <matt@ice-nine.org>
2025-11-26 15:53:41 -0600

    tabr, all: update sshbot framework, now scannerbots

--- openbsd-skel/update-scannerbots.sh
+++ openbsd-skel/update-scannerbots.sh
@@ -2,28 +2,28 @@
 # TODO: fix all these to support weird usernames and ipv6
 < /var/log/authlog \
   grep 'sshd.*Invalid user'|sed -es'%^.* from%%g' -es'% port.*$%%g' |\
-  xargs doas pfctl -t sshbots -T add
+  xargs doas pfctl -t scannerbots -T add
 doas zcat /var/log/authlog.* | \
   grep 'sshd.*Invalid user'|sed -es'%^.* from%%g' -es'% port.*$%%g' |\
-  xargs doas pfctl -t sshbots -T add
+  xargs doas pfctl -t scannerbots -T add
 < /var/log/authlog \
   awk '/Disconnected from invalid user/ {print $11}'| \
-  sort -n|uniq|grep -v '[[:alpha:]]'|xargs doas pfctl -t sshbots -T add
+  sort -n|uniq|grep -v '[[:alpha:]]'|xargs doas pfctl -t scannerbots -T add
 doas zcat /var/log/authlog.* | \
   awk '/Disconnected from invalid user/ {print $11}'| \
-  sort -n|uniq|grep -v '[[:alpha:]]'|xargs doas pfctl -t sshbots -T add
+  sort -n|uniq|grep -v '[[:alpha:]]'|xargs doas pfctl -t scannerbots -T add
 < /var/log/authlog \
   awk '/Disconnected from authenticating user/ {print $11}'| \
-  sort -n|uniq|grep -v '[[:alpha:]]'|xargs doas pfctl -t sshbots -T add
+  sort -n|uniq|grep -v '[[:alpha:]]'|xargs doas pfctl -t scannerbots -T add
 doas zcat /var/log/authlog.* | \
   awk '/Disconnected from authenticating user/ {print $11}'| \
-  sort -n|uniq|grep -v '[[:alpha:]]'|xargs doas pfctl -t sshbots -T add
+  sort -n|uniq|grep -v '[[:alpha:]]'|xargs doas pfctl -t scannerbots -T add
 < /var/log/authlog \
   awk '/Unable to negotiate/ {print $10}'| \
-  sort -n|uniq|grep -v '[[:alpha:]]'|xargs doas pfctl -t sshbots -T add
+  sort -n|uniq|grep -v '[[:alpha:]]'|xargs doas pfctl -t scannerbots -T add
 doas zcat /var/log/authlog.* | \
   awk '/Unable to negotiate/ {print $10}'| \
-  sort -n|uniq|grep -v '[[:alpha:]]'|xargs doas pfctl -t sshbots -T add
-doas pfctl -t sshbots -T delete -f sshbot-exclusions
-doas pfctl -t sshbots -T show > /tmp/t && \
-  doas mv /tmp/t /etc/pf.sshbots
+  sort -n|uniq|grep -v '[[:alpha:]]'|xargs doas pfctl -t scannerbots -T add
+doas pfctl -t scannerbots -T delete -f sshbot-exclusions
+doas pfctl -t scannerbots -T show > /tmp/t && \
+  doas mv /tmp/t /etc/pf.scannerbots
