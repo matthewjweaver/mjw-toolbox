9e224f2bfc9b7cd906f1ef01b478a4dd6fa0f447
matthewjweaver <matt@ice-nine.org>
2026-02-03 16:38:47 -0600

    hive: plex moves to taqwa

--- hive/create-plex.sh
+++ hive/create-plex.sh
@@ -2,26 +2,9 @@
 # If running for the first time, include the plex claim token:
 #  -e PLEX_CLAIM="XXXLOLXXX" \
 
+# doas adduser -h /home/plex -D -u 5001 plex
 
-# doas adduser -h /home/plex -D -u 501 plex
-
-if [ $(hostname) != p.hive.here ]; then
-  exit 1
-fi
-
-if [ ! -d /mistakenot ]; then
-  mkdir /mistakenot
-  echo "check fstab for nfs mounts!"
-  exit 1
-fi
-if [ ! -d /multi ]; then
-  mkdir /multi
-  echo "check fstab for nfs mounts!"
-  exit 1
-fi
-if [ ! -d /shadowhax-movies ]; then
-  mkdir /shadowhax-movies
-  echo "check fstab for nfs mounts!"
+if [ $(hostname) != taqwa ]; then
   exit 1
 fi
 
@@ -34,15 +17,15 @@ docker run \
   --name plex \
   --network=host \
   --restart always \
-  -e ADVERTISE_IP='http://192.168.223.41:32400/' \
+  -e ADVERTISE_IP='http://192.168.223.10:32400/' \
   -e ALLOWED_NETWORKS='192.168.223.0/24,10.42.0.0/16' \
   -e TZ="Etc/UTC" \
-  -e PUID="501" \
+  -e PUID="5001" \
   -e PGID="100" \
-  -v /home/plex:/config \
-  -v /mistakenot:/mistakenot \
-  -v /multi:/multi \
-  -v /shadowhax-movies:/shadowhax-movies \
+  -v /volume1/plex-config:/config \
+  -v /volume1/mistakenot:/mistakenot \
+  -v /volume1/multi:/multi \
+  -v /volume2/shadowhax-movies:/shadowhax-movies \
   -v /tmp:/transcode \
   ${CONTAINER}
 
